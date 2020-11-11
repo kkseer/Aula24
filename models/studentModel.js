d297dcbbbe4a4731d38e8b4c1708721772cfe4e8
carlos.silva <kkseer@gmail.com>
2020-11-11 08:19:59 +0100

    Remove the now ignored directory node_modules

---
+++ models/studentModel.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+
+const studentSchema = mongoose.Schema({
+    name: {
+        type: String,
+        require: true
+    },
+
+    subject: {
+        type: String,
+        require: true,
+    },
+
+    type: {
+        type: String,
+        require: true,
+    },
+    value: {
+        type: Number,
+        require: true,
+        //valida se a nota inserida e menor que zero
+        min: 0
+    },
+    lastModified: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const studentModel = mongoose.model('student', studentSchema, 'student');
+
+export {studentModel};
+
---
+++ routes/studentRouter.js
@@ -0,0 +1,60 @@
+import express from 'express';
+import { studentModel } from '../models/studentModel.js';
+
+const app = express();
+
+app.post('/student', async (req, res) => {
+  try {
+    const student = new studentModel(req.body);
+
+    await student.save();
+
+    res.send(student);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+//RETRIEVE
+app.get('/student', async (req, res) => {
+  try {
+    const student = await studentModel.find({});
+    res.send(student);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+//UPDATE
+app.patch('/student/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const student = await studentModel.findByIdAndUpdate(
+      { _id: id },
+      req.body,
+      { new: true }
+    );
+    res.send(student);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+//DELETE
+
+app.delete('/student/:id', async (req, res) => {
+  try {
+    const student = await studentModel.findByIdAndDelete({
+      _id: req.params.id,
+    });
+    if (!student) {
+      res.status(404).send('Documento nao encontrado na colecao');
+    } else {
+      res.send(200).send();
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+export { app as studentRouter };
