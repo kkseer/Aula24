33ece4ed3c038ded6d04540617b173b0bd68a4bd
carlos.silva <kkseer@gmail.com>
2020-11-11 11:18:42 +0100

    Arrumado require e acesso ao MongoDB

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import { studentRouter } from './routes/studentRouter.js';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 
 require('dotenv').config();
 
@@ -9,7 +11,7 @@ require('dotenv').config();
 (async () => {
   try {
     await mongoose.connect(
-      `mongodb+srv://{$process.env.USERDB}':'{$process.env.PWDDB}@cluster0.o2nvl.mongodb.net/grades?retryWrites=true&w=majority`
+      `mongodb+srv://${process.env.USERDB}:${process.env.PWDDB}@cluster0.o2nvl.mongodb.net/grades?retryWrites=true&w=majority`
     ),
       { useNewUrlParser: true, useUnifiedTopology: true };
     console.log('Conectado no MongoDB com sucesso');
