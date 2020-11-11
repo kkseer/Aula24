5a06fe0ed6d1ac563d4067bdd8cd3a715132eb25
carlos.silva <kkseer@gmail.com>
2020-11-11 08:08:42 +0100

    novo downloaded

---
+++ app.js
@@ -0,0 +1,25 @@
+// IMPORTS
+import express from 'express';
+import mongoose from 'mongoose';
+import { studentRouter } from './routes/studentRouter.js';
+
+require('dotenv').config();
+//CREATE
+(async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://{$process.env.USERDB}':'{$process.env.PWDDB}@cluster0.o2nvl.mongodb.net/grades?retryWrites=true&w=majority`
+    ),
+      { useNewUrlParser: true, useUnifiedTopology: true };
+    console.log('Conectado no MongoDB com sucesso');
+  } catch (error) {
+    console.log('erro ao conectar' + error);
+  }
+})();
+
+const app = express();
+
+app.use(express.json());
+app.use(studentRouter);
+
+app.listen(process.env.PORT, () => console.log('API iniciada'));
